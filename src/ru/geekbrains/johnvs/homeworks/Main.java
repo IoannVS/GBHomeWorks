d0c7449d24f71d013be6d42bb0248dbd49d95834
IoannVS <ioannvs10sirant@gmail.com>
2022-03-28 14:14:01 +0500

    Clean main

--- src/ru/geekbrains/johnvs/homeworks/Main.java
+++ src/ru/geekbrains/johnvs/homeworks/Main.java
@@ -1,28 +1,11 @@
 package ru.geekbrains.johnvs.homeworks;
 
-import java.util.Arrays;
-import java.util.Random;
-
 public class Main {
+
     public static void main(String[] args) {
 
-        int[] array = new int[5];
-        Random random = new Random();
-        for (int j = 0; j < array.length; j++) array[j] = random.nextInt(11);
-        boolean result = compare(array);
-        System.out.println(Arrays.toString(array));
-        System.out.println(result);
-    }
+        // Здесь будет меню выбора ДЗ из списка доступных
 
-    public static boolean compare(int[] arr) {
-        int sumLeft = 0, sumRight, sum=0;
-        for (int k : arr) sum += k;
-        for (int j : arr) {
-            sumLeft += j;
-            sumRight = sum - sumLeft;
-            if (sumLeft == sumRight) return true;
-        }
-        return false;
     }
 }
 
