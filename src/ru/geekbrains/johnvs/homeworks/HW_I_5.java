5063926beaac308cec0cfbada0816b9519b9f9bf
IoannVS <ioannvs10sirant@gmail.com>
2022-04-03 16:12:07 +0500

    UPD HW_I_5

--- src/ru/geekbrains/johnvs/homeworks/HW_I_5.java
+++ src/ru/geekbrains/johnvs/homeworks/HW_I_5.java
@@ -14,13 +14,14 @@ public class HW_I_5 {
                 79324567890L, 230000, 34);
         employees[1] = new Employee("Erika Jane Smith","Fullstack Developer", "ErikaJ@gmail.com",
                 79765563412L, 170000, 42);
-        employees[2] = new Employee("Abraham Wild","Mobile Developer", "Tiger1000@gmail.com",
-                79567345783L, 150000, 41);
-        employees[3] = new Employee("Julia West","Frontend Developer", "WestTexas1984@gmail.com",
-                79567876451L, 110000, 44);
-        employees[4] = new Employee("Stan Lee","Creative Designer", "DrawMan@gmail.com",
+        employees[2] = new Employee("Stan Lee","Creative Designer", "DrawMan@gmail.com",
                 79567876451L, 110000, 37);
+        employees[3] = new Employee("Abraham Wild","Mobile Developer", "Tiger1000@gmail.com",
+                79567345783L, 150000, 41);
+        employees[4] = new Employee("Julia Jessika West","Frontend Developer",
+                "WestTexas1984@gmail.com", 79567876451L, 110000, 44);
 
+        System.out.println("\tВыводим данные сотрудников старше 40 лет");
         for (Employee every: employees) {
             if (every.age > 40) {
                 every.showInfo();
